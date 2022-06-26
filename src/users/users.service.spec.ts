caf3e7b9d15eb41c8a78cbb5e05345173d73a678
Albin Ma <albinma@gmail.com>
2022-06-25 22:11:08 -0400

    Add test coverage for UserService

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -4,6 +4,7 @@ import { UsersService } from './users.service';
 import { getLoggerToken } from 'nestjs-pino';
 import { CryptoService } from '../common/security/crypto.service';
 import { Web3Service } from '../common/web3/web3.service';
+import { BadRequestException } from '@nestjs/common';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -15,11 +16,51 @@ describe('UsersService', () => {
         {
           provide: PrismaService,
           useValue: {
-            user: { findUnique: jest.fn() },
+            userWalletLogin: {
+              findUnique: jest.fn(val => {
+                if (
+                  val &&
+                  val.where &&
+                  val.where.publicAddress &&
+                  val.where.publicAddress === '0x8ba1f109551bD432803012645Ac136ddd64DBA72'
+                ) {
+                  return {
+                    publicAddress: val.where.publicAddress,
+                    nonce: 'nonce',
+                  };
+                }
+              }),
+            },
+            user: {
+              create: jest.fn(val => {
+                if (val && val.data && val.data.userWalletLogin) {
+                  return {
+                    ...val.data,
+                    userWalletLogin: {
+                      ...val.data.userWalletLogin.create,
+                    },
+                  };
+                }
+              }),
+              findUnique: jest.fn(val => {
+                if (val && val.where) {
+                  if (val.where.id === '123') {
+                    return { id: '123' };
+                  } else if (val.where.username === 'fujiwara_takumi') {
+                    return { username: 'fujiwara_takumi' };
+                  }
+                }
+              }),
+            },
           },
         },
         Web3Service,
-        CryptoService,
+        {
+          provide: CryptoService,
+          useValue: {
+            generateNonce: jest.fn(() => '123'),
+          },
+        },
         {
           provide: getLoggerToken(UsersService.name),
           useValue: null,
@@ -30,7 +71,106 @@ describe('UsersService', () => {
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should get user by id', async () => {
+    // arrange
+    const userId = '123';
+
+    // act
+    const user = await service.getUserById(userId);
+
+    // assert
+    expect(user).toBeDefined();
+    expect(user).not.toBeNull();
+    expect(user.id).toBe(userId);
+  });
+
+  it('should not get user by id', async () => {
+    // arrange
+    const userId = 'NOT_EXISTING_USER_ID';
+
+    // act
+    const user = await service.getUserById(userId);
+
+    // assert
+    expect(user).toBeUndefined();
+  });
+
+  it('should get username', async () => {
+    // arrange
+    const username = 'fujiwara_takumi';
+
+    // act
+    const user = await service.getUserByUsername(username);
+
+    // assert
+    expect(user).toBeDefined();
+    expect(user).not.toBeNull();
+    expect(user.username).toBe(username);
+  });
+
+  it('should not get username', async () => {
+    // arrange
+    const username = 'NOT_EXISTING_USERNAME';
+
+    // act
+    const user = await service.getUserByUsername(username);
+
+    // assert
+    expect(user).toBeUndefined();
+  });
+
+  it('should create web3 login', async () => {
+    // arrange
+    const address = '0x8ba1f109551bD432803012645Ac136ddd64DBA72';
+
+    // act
+    const userWalletLogin = await service.createWeb3Login(address);
+
+    // assert
+    expect(userWalletLogin).toBeDefined();
+    expect(userWalletLogin).not.toBeNull();
+    expect(userWalletLogin.publicAddress).toBe(address);
+    expect(userWalletLogin.nonce).toBe('123');
+  });
+
+  it('should throw invalid public adress on creating web3 login', async () => {
+    // arrange
+    const address = 'I like turtles!';
+
+    // actsert
+    await expect(service.createWeb3Login(address)).rejects.toThrow(BadRequestException);
+  });
+
+  it('should get wallet login by public address', async () => {
+    // arrange
+    const address = '0x8ba1f109551bD432803012645Ac136ddd64DBA72';
+
+    // act
+    const userWalletLogin = await service.getWalletLoginByPublicAddress(address);
+
+    // assert
+    expect(userWalletLogin).toBeDefined();
+    expect(userWalletLogin).not.toBeNull();
+    expect(userWalletLogin.publicAddress).toBe(address);
+    expect(userWalletLogin.nonce).toBe('nonce');
+  });
+
+  it('should fail to find wallet login', async () => {
+    // arrange
+    const address = '0x0Ac1dF02185025F65202660F8167210A80dD5086';
+
+    // act
+    const userWalletLogin = await service.getWalletLoginByPublicAddress(address);
+
+    // assert
+    expect(userWalletLogin).toBeUndefined();
+  });
+
+  it('should fail to find wallet login by public address because address is invalid', async () => {
+    // arrange
+    const address = 'I like turtles!';
+
+    // act
+    await expect(service.getWalletLoginByPublicAddress(address)).rejects.toThrow(BadRequestException);
   });
 });
