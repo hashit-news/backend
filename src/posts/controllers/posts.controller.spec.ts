627b9abb65ac9b70188ef2a12dd22fc919ff2b20
Albin Ma <albinma@gmail.com>
2022-07-27 23:30:18 -0400

    Fix unit tests

--- src/posts/controllers/posts.controller.spec.ts
+++ src/posts/controllers/posts.controller.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../../common/database/prisma.service';
+import { PostsService } from '../services/posts.service';
 import { PostsController } from './posts.controller';
 
 describe('PostsController', () => {
@@ -6,6 +8,13 @@ describe('PostsController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        PostsService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+      ],
       controllers: [PostsController],
     }).compile();
 
--- src/posts/services/posts.service.spec.ts
+++ src/posts/services/posts.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../../common/database/prisma.service';
 import { PostsService } from './posts.service';
 
 describe('PostsService', () => {
@@ -6,7 +7,13 @@ describe('PostsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
